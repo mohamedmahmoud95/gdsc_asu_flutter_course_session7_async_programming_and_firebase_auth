5ed248933b124521378e1da47d460730c5471bf9
Mohamed <m.raslan97@gmail.com>
2023-07-06 14:50:48 +0300

    added first page (for async programming code)

---
+++ lib/async_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AsyncPage extends StatefulWidget {
+  const AsyncPage({Key? key}) : super(key: key);
+
+  @override
+  State<AsyncPage> createState() => _AsyncPageState();
+}
+
+class _AsyncPageState extends State<AsyncPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
