d436bf5d626e5a88eb23fd3e015f9eb9429922f6
Mohamed <m.raslan97@gmail.com>
2023-07-06 15:09:19 +0300

    added illustration of async and await keywords

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,20 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  void getData(){
 
+  //Synchronous vs Asynchronous
+
+
+  /*
+  async: The async keyword is used to mark a function as asynchronous.
+  It allows the function to use the await keyword and indicates that the function may perform asynchronous operations.
+await: The await keyword can only be used within an async function.
+ It is used to pause the execution of the function until a future or a promise completes and returns a result.
+  When an await expression is encountered, the function is suspended,
+   and control is returned to the caller until the awaited operation is completed.
+   */
+
+  void getData(){
 
     Future.delayed(const Duration (seconds: 3), () {
       debugPrint("first print statement, after 3 seconds");
